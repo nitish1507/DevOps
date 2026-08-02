23fa648609d5d9f7c7d39f718f05ccf03e6242b2
nitish1507 <shiva11421@gmail.com>
2026-08-02 21:48:12 +0530

    Delete child_module directory

--- child_module/azurerm_network_security_group/output.tf
+++
@@ -1,6 +0,0 @@
-output "nsg_id" {
-  value = {
-    for k, v in azurerm_network_security_group.nsg :
-    k => v.id
-  }
-}
--- child_module/azurerm_resource_group/main.tf
+++
@@ -1,7 +0,0 @@
-variable "rgn" {}
-
-resource "azurerm_resource_group" "rg" {
-for_each = var.rgn
-  name     = each.value.name
-  location = each.value.location
-}
