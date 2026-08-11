dd18a4269f14aaedc2b9c5a016287cf489833264
nitish1507 <shiva11421@gmail.com>
2026-08-12 01:30:07 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,15 +1,15 @@
 module "resource_group" {
-  source = "../modules/azurerm_resource_group"
+  source = "../../modules/resource_group"
   rgn    = var.rgn
 }
 module "virtual_network" {
   depends_on = [module.resource_group]
-  source     = "../modules/azurerm_virtual_network"
+  source     = "../../modules/virtual_network"
   vnet       = var.vnet
 }
 module "subnet" {
   depends_on = [module.virtual_network]
-  source     = "../modules/azurerm_subnet"
+  source     = "../../modules/subnet"
   subnet     = var.subnet
 }
 module "public_ip" {
@@ -19,30 +19,30 @@ module "public_ip" {
 }
 module "virtual_machine" {
   depends_on = [module.subnet, module.public_ip]
-  source     = "../modules/azurerm_virtual_machine"
+  source     = "../../modules/azurerm_virtual_machine"
   machine    = var.machine
 }
 module "network_security_group" {
   depends_on = [module.resource_group]
-  source     = "../modules/azurerm_network_security_group"
+  source     = "../../modules/azurerm_network_security_group"
 
   nsg       = var.nsg
   nsg_rules = var.nsg_rules
 }
 module "bastion" {
   depends_on = [module.resource_group, module.subnet]
-  source     = "../modules/azurerm_bastion"
+  source     = "../../modules/azurerm_bastion"
   bastion    = var.bastion
 
 }
 module "keyvault" {
   depends_on = [module.resource_group]
-  source     = "../modules/azurerm_key_vault"
+  source     = "../../modules/azurerm_key_vault"
   keyvault   = var.keyvault
 
 }
 module "loadbalancer" {
   depends_on   = [module.resource_group]
-  source       = "../modules/azurerm_load_balancer"
+  source       = "../../modules/azurerm_load_balancer"
   loadbalancer = var.loadbalancer
 }
