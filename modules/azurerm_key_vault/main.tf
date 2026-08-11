e6aaf51055ae85910315f82db14dc9aaf8ec9a75
nitish1507 <shiva11421@gmail.com>
2026-08-12 01:52:24 +0530

    Update main.tf

--- modules/azurerm_key_vault/main.tf
+++ modules/azurerm_key_vault/main.tf
@@ -1,18 +1,22 @@
 variable "keyvault" {}
+
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "kv" {
   for_each = var.keyvault
+
   name                = each.value.name
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  sku_name  = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  rbac_authorization_enabled = false
+
   soft_delete_retention_days = 7
   purge_protection_enabled   = false
 
   access_policy {
-
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
@@ -26,5 +30,4 @@ resource "azurerm_key_vault" "kv" {
       "Restore"
     ]
   }
-
 }
