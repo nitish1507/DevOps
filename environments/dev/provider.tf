aeff42afccd587b25d0813d617b7da41f48fd516
nitish1507 <shiva11421@gmail.com>
2026-08-12 01:45:56 +0530

    Update provider.tf

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,12 +1,3 @@
 provider "azurerm" {
   features {}
 }
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Rgbackend"
-    storage_account_name = "stgbackendaccount"
-    container_name       = "mycontainer2"
-    key                  = "mycontainer.tfstate"
-  }
-}
