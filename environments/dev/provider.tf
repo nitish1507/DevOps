ddfa5be3734e8406663a5dc0c4dbe4408960541c
nitish1507 <shiva11421@gmail.com>
2026-08-15 23:14:37 +0530

    Configure Terraform backend for Azure

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -4,3 +4,11 @@ provider "azurerm" {
 
   use_oidc = true
 }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg_ball"
+    storage_account_name = "ballstg"
+    container_name       = "mycontainer2"
+    key                  = "mycontainer.tfstate"
+  }
+}
